746582f5300ba5dcf359c5e98f448a5493df4d21
Jaakko <jaakko@kronogra.fi>
2019-08-29 20:14:00 +0300

    added game engine library and made basic gameloop

--- DndTable/DnDTable/GameForm.cs
+++ DndTable/DnDTable/GameForm.cs
@@ -7,16 +7,58 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+using GameEngine;
 
 namespace DnDTable
 {
     public partial class GameForm : Form
-    {
+    {   
         public GameForm(Screen screen)
         {
             InitializeComponent();
+            Application.Idle += Application_Idle;
+
             Rectangle bounds = screen.Bounds;
             Bounds = bounds;
         }
+
+        void GameUpdate()
+        {
+        }
+
+        #region
+        //GameLoop DO NOT TOUCH
+        //NO EVEN TOUCHY TOUCHY
+        //*not sure why it works*
+        [StructLayout(LayoutKind.Sequential)]
+        public struct NativeMessage
+        {
+            public IntPtr Handle;
+            public uint Message;
+            public IntPtr WParameter;
+            public IntPtr LParameter;
+            public uint Time;
+            public Point Location;
+        }
+
+        [DllImport("user32.dll")]
+        public static extern int PeekMessage(out NativeMessage message, IntPtr window, uint filterMin, uint filterMax, uint remove);
+
+        private void Application_Idle(object sender, EventArgs e)
+        {
+            while (IsApplicationIdle())
+            {
+                GameUpdate();
+                Invalidate();
+            }
+        }
+        private bool IsApplicationIdle()
+        {
+            NativeMessage result;
+            return PeekMessage(out result, IntPtr.Zero, (uint)0, (uint)0, (uint)0) == 0;
+        }
+        #endregion
     }
 }
