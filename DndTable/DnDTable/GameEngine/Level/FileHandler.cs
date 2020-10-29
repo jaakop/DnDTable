bfb84be581df4c4b892fc50656f4e5877c5755b6
Jaakko <jaakko@kronogra.fi>
2020-10-29 11:17:57 +0200

    Documented FileHandler

--- DndTable/DnDTable/GameEngine/Level/FileHandler.cs
+++ DndTable/DnDTable/GameEngine/Level/FileHandler.cs
@@ -1,18 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO;
-using System.Runtime;
+﻿using System.IO;
 
 using Newtonsoft.Json;
 
+/// @author  Jaakko Sukuvaara
+/// @version 2020
 namespace GameEngine.Level
 {
+    /// <summary>
+    /// File Handler
+    /// </summary>
     public class FileHandler
     {
-
+        /// <summary>
+        /// Save a level to a file
+        /// </summary>
+        /// <param name="lvl">Level to be saved</param>
+        /// <param name="path">Path for the save file</param>
         public static void SaveLevel(Level lvl, string path)
         {
             Level level = new Level();
@@ -43,6 +46,12 @@ namespace GameEngine.Level
                 }
             }
         }
+
+        /// <summary>
+        /// Load a level from a file
+        /// </summary>
+        /// <param name="path">Path to file</param>
+        /// <returns>Level</returns>
         public static Level LoadLevel(string path)
         {
             Level level = JsonConvert.DeserializeObject<Level>(File.ReadAllText(path));
